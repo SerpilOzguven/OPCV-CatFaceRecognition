2856bf4a7b3047e6f613c89e3c8a3109a329a3dc
Serpil Özgüven <serpilozguven776@gmail.com>
2023-09-07 12:55:50 +0300

    kedi yüzü tanıma

---
+++ kedi_yuzu_tanima_projesi.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 16:19:28 2022
+
+@author: Serpil ÖZGÜVEN
+"""
+
+import cv2
+import os
+
+files = os.listdir()
+print (files)
+
+img_path_list = []
+
+for f in files:
+    if f.endswith(".jpg"):
+        img_path_list.append(f)
+print(img_path_list)
+
+
+for j in img_path_list:
+    print(j)
+    image = cv2.imread(j)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    detector  = cv2.CascadeClassifier("haarcascade_frontalcatface.xml")
+    
+    rects = detector.detectMultiScale(gray, scaleFactor = 1.042, minNeighbors = 2)
+    
+    
+    for (i, (x,y,w,h)) in enumerate(rects):
+        cv2.rectangle(image, (x,y), (x+w, y+h),(0,255,255),2)
+        cv2.putText(image, "Kedi {}".format(i+1), (x,y-10),cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0,255,255),2)
+
+
+
+    cv2.imshow(j, image)
+    if    cv2.waitKey(0) & 0xFF == ord("q"): continue
+    
